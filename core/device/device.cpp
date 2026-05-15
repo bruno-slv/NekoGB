fc3949690e6928de6d6ae5c6cd3e40b85c6fe34a
bruno-slv <obrunosantos11@gmail.com>
2026-05-15 00:27:12 -0300

    [DEVICE] Address logic removed, will be implemented on BUS

--- core/device/device.cpp
+++ core/device/device.cpp
@@ -1,7 +1,6 @@
 #include <device.h>
 
-IDevice::IDevice(u16 start, u16 end)
-    : address{start, end}
+IDevice::IDevice()
 {
 }
 
--- core/device/device.h
+++ core/device/device.h
@@ -3,25 +3,16 @@
 
 #include <neko_types.h>
 
-typedef struct
-{
-    u16 start;
-    u16 end;
-} AddressRange;
-
 class IDevice
 {
-protected:
-    AddressRange address;
-
 public:
     virtual void tick(u32 cycles) = 0;
     virtual u8 reset() = 0;
 
-    virtual u8 read(u16 address) = 0;
-    virtual void write(u16 address, u8 value) = 0;
+    virtual u8 read(u16 offset) = 0;
+    virtual void write(u16 offset, u8 value) = 0;
 
-    IDevice(u16 start, u16 end);
+    IDevice();
 
     virtual ~IDevice();
 };
